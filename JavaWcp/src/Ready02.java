602f9a65a72da1c0d5a718ea8010bbfa0500dae4
福田優美 <Yumi@fukudayumi-no-MacBook-Air.local>
2021-10-14 15:41:10 +0900

    finish 2

--- JavaWcp/src/Ready02.java
+++ JavaWcp/src/Ready02.java
@@ -1,6 +1,21 @@
 
 public class Ready02 {
-	 public static void main(String[] args) {
-	        System.out.println("Hello World!");
-	    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		int number = 12;
+
+		// switch文を用いて、numberを3で割った余りで条件分岐をしてください
+		switch (number % 3) {
+		case 0:
+			System.out.println("3で割り切れます");
+			break;
+		case 1:
+			System.out.println("3で割ると1余ります");
+			break;
+		case 2:
+			System.out.println("3で割ると2余ります");
+			break;
+		}
+	}
 }
