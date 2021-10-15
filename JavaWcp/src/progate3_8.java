dbd8028cf83856089b0dc14949ef11d13a599a16
福田優美 <Yumi@fukudayumi-no-MacBook-Air.local>
2021-10-16 08:41:00 +0900

    almost finish part of 5

--- JavaWcp/src/progate3_8.java
+++ JavaWcp/src/progate3_8.java
@@ -1,75 +1,72 @@
-
-public class progate3_8 {
-	public static void main(String[] args) {
-		printData(fullName("Kate", "Jones"), 27, 1.6, 50.0);
-		printData(fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
-	}
-
-	public static void printData(String name, int age, double height, double weight) {
-		System.out.println("私の名前は" + name + "です");
-		System.out.println("年齢は" + age + "歳です");
-		System.out.println("身長は" + height + "mです");
-		System.out.println("体重は" + weight + "kgです");
-		double bmi = bmi(height, weight);
-		System.out.println("BMIは" + bmi + "です");
-
-		// isHealthyメソッドの結果で条件分岐を行ってください
-		if (isHealthy(bmi)) {
-			System.out.println("健康です");
-		} else {
-			System.out.println("健康ではありません");
-		}
-
-	}
-
-	public static String fullName(String firstName, String lastName) {
-		return firstName + " " + lastName;
-	}
-
-	public static String fullName(String firstName, String middleName, String lastName) {
-		return firstName + " " + middleName + " " + lastName;
-	}
-
-	public static double bmi(double height, double weight) {
-		return weight / height / height;
-	}
-
-	// isHealthyメソッドを定義してください
-	public static boolean isHealthy(double bmi) {
-		return bmi >= 18.5 && bmi < 25.0;
-	}
-	
-	import java.util.Scanner;
-
-	class Main {
-	  public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    
-	    System.out.print("名前：");
-	    // 文字列の入力を受け取ってください
-	    String firstName = scanner.next();
-	    
-	    System.out.print("名字：");
-	    // 文字列の入力を受け取ってください
-	    String lastName = scanner.next();
-	    
-	    System.out.print("年齢：");
-	    // 整数の入力を受け取ってください
-	    int age = scanner.nextInt();
-	    
-	    System.out.print("身長(m)：");
-	    // 小数の入力を受け取ってください
-	    double height =  scanner.nextDouble();
-	    
-	    System.out.print("体重(kg)：");
-	    // 小数の入力を受け取ってください
-	    double weight = scanner.nextDouble();
-	    
-	    Person.printData(Person.fullName(firstName, lastName), age, height, weight);
-	  }
-	}
-
-
-
-
-}
+//
+//public class progate3_8 {
+//	public static void main(String[] args) {
+//		printData(fullName("Kate", "Jones"), 27, 1.6, 50.0);
+//		printData(fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
+//	}
+//
+//	public static void printData(String name, int age, double height, double weight) {
+//		System.out.println("私の名前は" + name + "です");
+//		System.out.println("年齢は" + age + "歳です");
+//		System.out.println("身長は" + height + "mです");
+//		System.out.println("体重は" + weight + "kgです");
+//		double bmi = bmi(height, weight);
+//		System.out.println("BMIは" + bmi + "です");
+//
+//		// isHealthyメソッドの結果で条件分岐を行ってください
+//		if (isHealthy(bmi)) {
+//			System.out.println("健康です");
+//		} else {
+//			System.out.println("健康ではありません");
+//		}
+//
+//	}
+//
+//	public static String fullName(String firstName, String lastName) {
+//		return firstName + " " + lastName;
+//	}
+//
+//	public static String fullName(String firstName, String middleName, String lastName) {
+//		return firstName + " " + middleName + " " + lastName;
+//	}
+//
+//	public static double bmi(double height, double weight) {
+//		return weight / height / height;
+//	}
+//
+//	// isHealthyメソッドを定義してください
+//	public static boolean isHealthy(double bmi) {
+//		return bmi >= 18.5 && bmi < 25.0;
+//	}
+//
+////import java.util.Scanner;
+////
+////class Main {
+////	public static void main(String[] args) {
+////		Scanner scanner = new Scanner(System.in);
+////
+////		System.out.print("名前：");
+////		// 文字列の入力を受け取ってください
+////		String firstName = scanner.next();
+////
+////		System.out.print("名字：");
+////		// 文字列の入力を受け取ってください
+////		String lastName = scanner.next();
+////
+////		System.out.print("年齢：");
+////		// 整数の入力を受け取ってください
+////		int age = scanner.nextInt();
+////
+////		System.out.print("身長(m)：");
+////		// 小数の入力を受け取ってください
+////		double height = scanner.nextDouble();
+////
+////		System.out.print("体重(kg)：");
+////		// 小数の入力を受け取ってください
+////		double weight = scanner.nextDouble();
+////
+////		Person.printData(Person.fullName(firstName, lastName), age, height, weight);
+////	}
+//}
+//
+//}
--- JavaWcp/src/progate4_4.java
+++ JavaWcp/src/progate4_4.java
@@ -1,4 +1,23 @@
 
 public class progate4_4 {
 
+	
+	class Person {
+		  public String name;
+
+		  // コンストラクタがString型の引数を受け取るようにしてください
+		  Person(String name) {
+		    System.out.println("インスタンスが生成されました");
+		    // インスタンスフィールドnameに値をセットしてください
+		    this.name  = name;
+		    
+		  }
+
+		  public void hello() {
+		    System.out.println("こんにちは、私は" + this.name + "です");
+		  }
+		  
+		  
+		}
+	
 }
