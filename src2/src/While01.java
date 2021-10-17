f15473c508b824423392782009e0c08904406933
saru1439s7 <d.n.saru39@gmail.com>
2021-10-18 08:00:30 +0900

    start sukkiriwakaru Java Nyuumonn

---
+++ src2/src/While01.java
@@ -0,0 +1,17 @@
+
+public class While01 {
+
+	public static void main(String[] args) {
+//		int number = 1;
+//		while (number <= 20) {
+//			number *= 2;
+//			System.out.println(number);
+//		}
+//
+//		int number = 1;
+//        do {
+//            number *= 2;
+//            System.out.println("While01 = " + number);
+//        } while (number < 50);
+	}
+}
