4413ccaf4e3eb980cb0c4b45cd983ef51ef1f40b
saru1439s7 <d.n.saru39@gmail.com>
2021-10-19 12:34:08 +0900

    add chapter8

---
+++ src2/src/Chapter8.java
@@ -0,0 +1,17 @@
+
+public class Chapter8 {
+	public static void main(String[] args) {
+		
+		int number = 1;
+		while (number <50) {
+			number*=2;
+			System.out.println(""+number);
+		}
+		int number2 = 1;
+		do {
+			number2*=2;
+			System.out.println(number2);
+		}while(number2<50);
+	}
+
+}
