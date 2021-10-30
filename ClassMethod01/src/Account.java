e34dd660658121701b1fa8055c2579242c2c616a
saru1439s7 <d.n.saru39@gmail.com>
2021-10-30 23:08:17 +0900

    fix

--- ClassMethod01/src/Account.java
+++ ClassMethod01/src/Account.java
@@ -34,11 +34,5 @@ public class Account {
 		}
 		return folder + file;
 	}
-	public String concatPath(String folder,String file) {
-		if (!folder.endsWith("¥¥")) {
-			folder += "¥¥";
-		}
-		return folder + file;
-	}
 
 }
