0aa4f314a57654c23b4a1d54898145ff9be160bc
saru1439s7 <d.n.saru39@gmail.com>
2021-11-23 08:57:16 +0900

    rensyuu

--- Sikenntaisaku/src/Main.java
+++ Sikenntaisaku/src/Main.java
@@ -173,6 +173,141 @@ public class Main {
 
 		// 変数boolの値を出力してください
 		System.out.println(bool);
+		System.out.println(4 + 2 > 6);
 
+		// 「4 + 2」と「6」を比較し、trueとなるようにしてください
+		System.out.println(4 + 2 <= 6);
+		// trueと出力されるようにしてください
+		System.out.println(true || false);
+
+		// falseと出力されるようにしてください
+		System.out.println(false && true);
+
+		// 「8 < 5」かつ「3 >= 2」の結果を出力してください
+		System.out.println(8 < 5 && 3 >= 2);
+
+		// 「8 < 5」または「3 >= 2」の結果を出力してください
+		System.out.println(8 < 5 || 3 >= 2);
+
+		// 「8 < 5」でない、の結果を出力してください
+		System.out.println(!(8 < 5));
+
+		if (true) {
+			System.out.println("条件式がtrueのため、出力されます");
+		}
+
+		// 条件式に直接falseをいれてください
+		if (false) {
+			System.out.println("条件式がfalseのため、出力されません");
+		}
+
+		int x = 5;
+		// if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力してください
+		if (2 < x) {
+			System.out.println("xは2より大きい");
+		}
+		if (x > 8) {
+			System.out.println("xは8より大きい");
+		}
+
+		// if文を用いて、変数xが8より大きいとき「xは8より大きい」と出力してください
+		int number = 12;
+
+		// numberが20より小さいとき、どちらでもないときの条件分岐を追加してください
+		if (number < 10) {
+			System.out.println("10より小さい");
+		} else if (number < 20) {
+			System.out.println("10以上、20より小さい");
+		} else {
+			System.out.println("20以上");
+		}
+		int number = 12;
+
+		// switch文を用いて、numberを3で割った余りで条件分岐をしてください
+		switch (number % 3) {
+		case 0:
+			System.out.println("3で割り切れます");
+			break;
+		case 1:
+			System.out.println("3で割ると1余ります");
+			break;
+		case 2:
+			System.out.println("3で割ると2余ります");
+			break;
+		}
+		switch (number % 5) {
+		case 0:
+			System.out.println("大吉です");
+			break;
+		case 1:
+			System.out.println("中吉です");
+			break;
+		case 4:
+			System.out.println("凶です");
+			break;
+		default:
+			System.out.println("吉です");
+			break;
+		}
+		int number = 10;
+
+		// while文を用いて、numberが0より大きい場合に繰り返す、繰り返し処理を作ってください
+		while (number > 0) {
+			System.out.println(number);
+			number--;
+		}
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + "回目のループです");
+		}
+		int i = 1;
+		while (i < 10) {
+			// iが5の倍数のとき、繰り返し処理を終了してください
+			if (i % 5 == 0) {
+				break;
+			}
+			System.out.println(i);
+			i++;
+		}
+
+		System.out.println("=== for文 ===");
+		for (int j = 1; j < 10; j++) {
+			// jが3の倍数のとき、処理をスキップしてください
+			if (j % 3 == 0) {
+				continue;
+			}
+
+			System.out.println(j);
+		}
+		String[] names = { "にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ" };
+
+		// インデックス番号が0の要素を出力してください
+		System.out.println(names[0]);
+
+		// インデックス番号が2の要素を出力してください
+		System.out.println(names[2]);
+		String[] names = { "にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ" };
+
+		// for文を用いて、「私の名前は◯◯です」と出力してください
+		for (int i = 0; i < names.length; i++) {
+			System.out.println("私の名前は" + names[i] + "です");
+		}
+		for(String name:names){
+		      System.out.println("私の名前は"+name+"です");
+		    }
+
+		int[] numbers={1, 4, 6, 9, 13, 16};
+
+	    int oddSum = 0;
+	    int evenSum = 0;
+
+	    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
+	    for(int number:numbers){
+	      if(number%2==0){
+	        evenSum+=number;
+
+	      }else{
+	        oddSum+=number;
+	      }
 	}
 }
