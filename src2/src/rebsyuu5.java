4b41b4fbaf0bfc6365f3b0c36e2304fd87c35b11
saru1439s7 <d.n.saru39@gmail.com>
2021-12-01 19:20:48 +0900

    rensyuu　fix

--- src2/src/rebsyuu5.java
+++ src2/src/rebsyuu5.java
@@ -9,13 +9,6 @@ public class rebsyuu5 {
 		mail(adress,title,body);
 	}
 
-	public static void introduceOneself() {
-		String name = "MAsa";
-		int age =50;
-		System.out.println(name);
-		System.out.println("年齢"+age);
-
-	}
 	public static void mail(String adress,String title,String body) {
 		System.out.println(adress);
 		System.out.println("タイトル"+title+"内容"+body);
