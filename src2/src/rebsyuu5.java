1d4b77497ba7f8310c29fadb852dc592291bec96
saru1439s7 <d.n.saru39@gmail.com>
2021-12-02 15:28:27 +0900

    rensyuu　fix

--- src2/src/rebsyuu5.java
+++ src2/src/rebsyuu5.java
@@ -2,17 +2,27 @@ public class rebsyuu5 {
 	// int型配列を受け取り、
 	// 配列内の要素すべてに1を加えるメソッド
 	//public static void incArray(int[] array) {
-	public static int[] makeArray(int size) {
-	    int[] newArray = new int[size];
-	    for (int i = 0; i < newArray.length; i++ ) {
-	      newArray[i] = i;
-	    }
-	    return newArray;
-	  }
-	  public static void main(String[] args) {
-	    int[] array = makeArray(3);
-	    for (int i : array) {
-	      System.out.println(i);
-	    }
-	  }
+	//	public static int[] makeArray(int size) {
+	//	    int[] newArray = new int[size];
+	//	    for (int i = 0; i < newArray.length; i++ ) {
+	//	      newArray[i] = i;
+	//	    }
+	//	    return newArray;
+	//	  }
+	//	  public static void main(String[] args) {
+	//	    int[] array = makeArray(3);
+	//	    for (int i : array) {
+	//	      System.out.println(i);
+	//	    }
+	//	  }
+
+	public static void main(String[] args) {
+		String adress = "p＠p";
+		String text = "こんにちは";
+		email(adress, text);
+	}
+
+	public static void email(String adress, String text) {
+		System.out.println(adress + "から以下ないよう" + text);
+	}
 }
