62796e41840ade3fb9bdc88ffe8d5098c7c5a834
saru1439s7 <d.n.saru39@gmail.com>
2021-12-02 16:51:09 +0900

    https://education.r-jc.jp/?p=1694 の問題

--- src2/src/rebsyuu5.java
+++ src2/src/rebsyuu5.java
@@ -1,28 +1,77 @@
 public class rebsyuu5 {
-	// int型配列を受け取り、
-	// 配列内の要素すべてに1を加えるメソッド
-	//public static void incArray(int[] array) {
-	//	public static int[] makeArray(int size) {
-	//	    int[] newArray = new int[size];
-	//	    for (int i = 0; i < newArray.length; i++ ) {
-	//	      newArray[i] = i;
-	//	    }
-	//	    return newArray;
-	//	  }
-	//	  public static void main(String[] args) {
-	//	    int[] array = makeArray(3);
-	//	    for (int i : array) {
-	//	      System.out.println(i);
-	//	    }
-	//	  }
-
+	//	1～20までの偶数の合計値を求め、その時に何回計算を行ったか計算回数を表示するプログラムを作成しなさい。
 	public static void main(String[] args) {
-		String adress = "p＠p";
-		String text = "こんにちは";
-		email(adress, text);
-	}
+		int sum = 0;
+		int count = 0;
+		for (int i = 1; i < 21; i++) {
+			if (i % 2 == 0) {
+				sum += i;
+				count++;
+			}
+		}
+		System.out.println("1～20までの偶数の合計値" + sum + "その時に" + count + "回計算を行った");
 
-	public static void email(String adress, String text) {
-		System.out.println(adress + "から以下ないよう" + text);
 	}
 }
+//public static void main(String[] args) {
+//	int count = 0;
+//	for (int i = 1; i < 11; i++) {
+//		if (i % 2 == 0) {
+//			count ++;
+//		}
+//	}
+//	System.out.println(count);
+//}
+//	//0～1まで『10,9,8(途中省略）,1』と表示させるプログラムを作成しなさい。
+//	public static void main(String[] args) {
+//		for (int i=10;i>0;i--) {
+//			if(i==1) {
+//				System.out.print(i);
+//				break;
+//			}
+//			System.out.print(i+",");
+//		}
+//	}
+//}
+//	public static void main(String[] args) {
+//		double triangleArea = calcTriangleArea(10.0, 5.0);
+//	    System.out.println("三角形の面積：" + triangleArea + "平方cm");
+//	    double circleArea = calcCircleArea(5.0);
+//	    System.out.println("円の面積：" + circleArea + "平方cm");
+//	    
+//	}
+//	public static double calcTriangleArea(double bottom,double height) {
+//		double area = 	(bottom*height)/2;
+//		return area;
+//	}
+//	public static double calcCircleArea(double radius) {
+//		double area = radius*radius*3.14;
+//		return area;
+//	}
+//}
+// int型配列を受け取り、
+// 配列内の要素すべてに1を加えるメソッド
+//public static void incArray(int[] array) {
+//	public static int[] makeArray(int size) {
+//	    int[] newArray = new int[size];
+//	    for (int i = 0; i < newArray.length; i++ ) {
+//	      newArray[i] = i;
+//	    }
+//	    return newArray;
+//	  }
+//	  public static void main(String[] args) {
+//	    int[] array = makeArray(3);
+//	    for (int i : array) {
+//	      System.out.println(i);
+//	    }
+//	  }
+//
+//	public static void main(String[] args) {
+//		String adress = "p＠p";
+//		String text = "こんにちは";
+//		email(adress, text);
+//	}
+//
+//	public static void email(String adress, String text) {
+//		System.out.println(adress + "から以下ないよう:" + text);
+//	}
