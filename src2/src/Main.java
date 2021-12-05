158359814e5a3dd7a0cbef37a07f6ad7ee0a0436
saru1439s7 <d.n.saru39@gmail.com>
2021-12-05 22:12:58 +0900

    rensyuu　java すっきりわかる

--- src2/src/Main.java
+++ src2/src/Main.java
@@ -3,7 +3,6 @@ import comment.Zenhan;
 public class Main {
   public static void main(String[] args) throws Exception {
     Zenhan.doWarusa();
-    Zenhan.doTogame();
     comment.Kouhan.callDeae();
     comment.Kouhan.showMondokoro();
   }
--- src2/src/comment/Kouhan.java
+++ src2/src/comment/Kouhan.java
@@ -8,6 +8,7 @@ public class Kouhan {
 	public static void showMondokoro() throws Exception {
 		System.out.println("飛車さん、角さん。もういいでしょう。");
 		System.out.println("この紋所が目にはいらぬか！");
+		Thread.sleep(7000);
 		Zenhan.doTogame(); // もう一度、とがめる
 	}
 }
